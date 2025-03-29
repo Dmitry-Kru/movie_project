c3250dd77057f6260c84c51d7e3fc86692a8012f
Dmitry-Kru <dukeingm@gmail.com>
2025-03-29 09:26:48 +0300

    2

--- movie_project/films/urls.py
+++ movie_project/films/urls.py
@@ -6,4 +6,5 @@ app_name = 'films'
 urlpatterns = [
     path('add/', views.add_film, name='add_film'),
     path('list/', views.films_list, name='films_list'),
+    path('', views.films_list, name='home'),
 ]
